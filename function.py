96f518e80969ccd4fe9acffeeb2373963040bb26
Flavio Fontes <flavio.carvalho.fontes@gmail.com>
2024-08-21 11:20:37 -0300

    Comentarios

--- function.py
+++ function.py
@@ -7,54 +7,65 @@ import glob
 
 class Function:
     def __init__(self) -> None:
+        # Inicializa o browser e o driver, e prepara as ações do mouse/teclado (ActionChains)
         self.browser = Browser()
         self.driver = self.browser.driver()
         self.action = ActionChains(driver=self.driver)
 
     def url(self, url: str):
+        # Carrega a página web especificada pela URL
         self._url = self.driver.get(url=url)
 
     def xpath(self, time: int, name: str):
+        # Aguarda até que o elemento identificado pelo XPath esteja presente na página e o retorna
         self.elemento = WebDriverWait(self.driver, int(time)).until(
             EC.presence_of_element_located((By.XPATH, name))
         )
         return self.elemento
 
     def css(self, time: int, name: str):
+        # Aguarda até que o elemento identificado pelo seletor CSS esteja presente na página e o retorna
         self.elemento = WebDriverWait(self.driver, int(time)).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, name))
         )
         return self.elemento
 
     def tag(self, time: int, name: str):
+        # Aguarda até que o elemento identificado pelo nome da tag esteja presente na página e o retorna
         self.elemento = WebDriverWait(self.driver, int(time)).until(
             EC.presence_of_element_located((By.TAG_NAME, name))
         )
         return self.elemento
 
     def class_name(self, time: int, name: str):
+        # Aguarda até que o elemento identificado pelo nome da classe esteja presente na página e o retorna
         self.elemento = WebDriverWait(self.driver, int(time)).until(
             EC.presence_of_element_located((By.CLASS_NAME, name))
         )
         return self.elemento
 
     def digitar(self, texto: str, elemento):
+        # Simula a digitação de texto em um elemento usando ActionChains
         self.action.move_to_element(elemento)
         self.action.click(elemento)
         self.action.send_keys(texto)
         self.action.perform()
 
     def digitar_js(self, texto: str, elemento):
+        # Insere texto em um elemento usando JavaScript
         self.driver.execute_script(f"arguments[0].value='{texto}';", elemento)
 
     def click(self, elemento):
+        # Simula um clique em um elemento usando ActionChains
         self.action.move_to_element(elemento)
         self.action.click(elemento)
         self.action.perform()
 
     def click_js(self, elemento):
+        # Simula um clique em um elemento usando JavaScript
         self.driver.execute_script("arguments[0].click();", elemento)
 
     def check_downloaded(self, download_dir: str, extension: str) -> bool:
+        # Verifica se há arquivos baixados com a extensão especificada no diretório de downloads
         files = glob.glob(f"{download_dir}/*{extension}")
-        return len(files) > 0
+        return len(files) > 0  # Retorna True se encontrar arquivos, caso contrário, False
--- webdriver.py
+++ webdriver.py
@@ -3,41 +3,53 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from pathlib import Path
 
-
 class Browser:
     def __init__(self):
+        # Inicializa o driver do navegador ao instanciar a classe Browser
         self._driver = self._initialize_driver()
 
     def _initialize_driver(self):
+        # Instala o ChromeDriver usando o webdriver_manager e define o serviço do ChromeDriver
         servico = Service(ChromeDriverManager().install())
+        
+        # Configurações do Chrome para o driver
         chrome_options = webdriver.ChromeOptions()
+        
+        # Define o caminho para o diretório de download
         caminho_download = Path(__file__).parent
+        
+        # Configurações de preferências para o navegador
         prefs = {
-            "download.default_directory": str(caminho_download.resolve()),
-            "download.Prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "safebrowsing.enabled": True,
-            "profile.default_content_setting_values.media_stream_mic": 1,
-            "profile.default_content_setting_values.media_stream_camera": 1,
-            "profile.default_content_setting_values.geolocation": 1,
-            "profile.default_content_setting_values.notifications": 1,
-            "credentials_enable_service": False,
-            "profile.password_manager_enabled": False,
+            "download.default_directory": str(caminho_download.resolve()),  # Diretório de download padrão
+            "download.Prompt_for_download": False,  # Não perguntar onde salvar o arquivo
+            "download.directory_upgrade": True,  # Permitir upgrade do diretório de download
+            "safebrowsing.enabled": True,  # Habilitar proteção contra sites maliciosos
+            "profile.default_content_setting_values.media_stream_mic": 1,  # Permitir uso do microfone
+            "profile.default_content_setting_values.media_stream_camera": 1,  # Permitir uso da câmera
+            "profile.default_content_setting_values.geolocation": 1,  # Permitir uso da geolocalização
+            "profile.default_content_setting_values.notifications": 1,  # Permitir notificações
+            "credentials_enable_service": False,  # Desabilitar o serviço de credenciais do Chrome
+            "profile.password_manager_enabled": False,  # Desabilitar o gerenciador de senhas do Chrome
         }
 
+        # Adiciona as preferências ao ChromeOptions
         chrome_options.add_experimental_option("prefs", prefs)
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option("useAutomationExtension", False)
-        chrome_options.add_argument("start-maximized")
-        chrome_options.add_argument("--disable-software-rasterizer")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")  # linux only
-        chrome_options.add_argument("--log-level=3")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--allow-running-insecure-content")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Oculta a automação
+        chrome_options.add_experimental_option("useAutomationExtension", False)  # Desativa a extensão de automação
+        
+        # Argumentos adicionais para o Chrome
+        chrome_options.add_argument("start-maximized")  # Inicia o navegador maximizado
+        chrome_options.add_argument("--disable-software-rasterizer")  # Desativa rasterização por software
+        chrome_options.add_argument("--disable-gpu")  # Desativa o uso de GPU
+        chrome_options.add_argument("--disable-dev-shm-usage")  # Desativa o uso de /dev/shm
+        chrome_options.add_argument("--no-sandbox")  # Necessário para Linux, desativa o sandbox
+        chrome_options.add_argument("--log-level=3")  # Configura o nível de log para 3 (erros apenas)
+        chrome_options.add_argument("--ignore-certificate-errors")  # Ignora erros de certificado
+        chrome_options.add_argument("--allow-running-insecure-content")  # Permite conteúdo inseguro
 
+        # Retorna uma instância do Chrome WebDriver com as opções configuradas
         return webdriver.Chrome(service=servico, options=chrome_options)
 
     def driver(self):
+        # Retorna o driver do navegador para uso externo
         return self._driver
